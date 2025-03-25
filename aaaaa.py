68e03fdc54208f42fd09aa37c0b49cf1e84e8d1c
marcelohq <marcelomhq2@gmail.com>
2025-03-25 17:07:03 -0300

    Iniciante completo

--- aaaaa.py
+++ aaaaa.py
@@ -1,16 +1,5 @@
-# x = int(input("Digite um número: "))
-# print(f"Números pares de 0 até {x}:")
-# for i in range(0, x + 1):
-#     if i % 2 == 0:
-#         print(i)
-       
-x = [1,2,44,522,25,63,11,2311,244]
-pares = []
-for i in x:
-  if i%2==0:
-    pares.append(i)
-    
+from collections import deque
 
-soma=(sum(pares))
-media=(soma/len(pares))
-print(f'a médias dos números pares é {media}')
+lista = deque([1,2,3,4,5])
+lista.popleft()
+print(lista)
